ec104325d5727595a22e03a27d7fc96e0aa36087
wx <wx@DESKTOP-CAI2JUT>
2018-09-01 15:16:04 +0800

    惹祸5千赫

--- shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
+++ shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/LoginLogoutTest.java
@@ -7,7 +7,10 @@ package com.github.zhangkaitao.shiro.chapter2;
  * <p>Date: 14-1-25
  * <p>Version: 1.0
  */
-public class LoginLogoutTest {5445y
+public class LoginLogoutTest {
 
-   afwfwqffffffffffffffffffffffffgntsyjnsrt
+ public static void main(String[] args) {
+	System.out.println("测试");
+	System.out.println("测试");
+}
 }
